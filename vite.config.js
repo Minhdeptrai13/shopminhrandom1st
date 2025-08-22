c4114b245d2342a87f067b6f3ad591527afc86ce
Minhdeptrai13 <binhminhnguvubinhminh@gmail.com>
2025-08-22 22:28:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [], // ❌ bỏ trống, không cần entry
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
